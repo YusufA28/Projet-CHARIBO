435378d57e90b9c2600a177954a2cccfe515e723
waffeulz <yus.akinci@gmail.com>
2017-11-29 13:30:59 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonBleu = new System.Windows.Forms.Button();
+            this.buttonRouge = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonrst = new System.Windows.Forms.Button();
+            this.buttonRose = new System.Windows.Forms.Button();
+            this.buttonVert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.fontDialog2 = new System.Windows.Forms.FontDialog();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tpsjeu = new System.Windows.Forms.Timer(this.components);
+            this.intervallecouleur = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonBleu
+            // 
+            this.buttonBleu.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonBleu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonBleu.Location = new System.Drawing.Point(6, 19);
+            this.buttonBleu.Name = "buttonBleu";
+            this.buttonBleu.Size = new System.Drawing.Size(205, 105);
+            this.buttonBleu.TabIndex = 0;
+            this.buttonBleu.Text = "Bleu";
+            this.buttonBleu.UseVisualStyleBackColor = false;
+            this.buttonBleu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonRouge
+            // 
+            this.buttonRouge.BackColor = System.Drawing.Color.Red;
+            this.buttonRouge.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRouge.Location = new System.Drawing.Point(290, 19);
+            this.buttonRouge.Name = "buttonRouge";
+            this.buttonRouge.Size = new System.Drawing.Size(199, 105);
+            this.buttonRouge.TabIndex = 2;
+            this.buttonRouge.Text = "Rouge";
+            this.buttonRouge.UseVisualStyleBackColor = false;
+            this.buttonRouge.Click += new System.EventHandler(this.buttonRouge_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonrst);
+            this.groupBox1.Controls.Add(this.buttonRose);
+            this.groupBox1.Controls.Add(this.buttonVert);
+            this.groupBox1.Controls.Add(this.buttonRouge);
+            this.groupBox1.Controls.Add(this.buttonBleu);
+            this.groupBox1.Location = new System.Drawing.Point(30, 213);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(497, 258);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // buttonrst
+            // 
+            this.buttonrst.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonrst.Location = new System.Drawing.Point(191, 118);
+            this.buttonrst.Name = "buttonrst";
+            this.buttonrst.Size = new System.Drawing.Size(113, 43);
+            this.buttonrst.TabIndex = 8;
+            this.buttonrst.Text = "Ok!";
+            this.buttonrst.UseVisualStyleBackColor = true;
+            this.buttonrst.Click += new System.EventHandler(this.buttonrst_Click);
+            // 
+            // buttonRose
+            // 
+            this.buttonRose.BackColor = System.Drawing.Color.DeepPink;
+            this.buttonRose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRose.Location = new System.Drawing.Point(6, 149);
+            this.buttonRose.Name = "buttonRose";
+            this.buttonRose.Size = new System.Drawing.Size(205, 103);
+            this.buttonRose.TabIndex = 4;
+            this.buttonRose.Text = "Rose";
+            this.buttonRose.UseVisualStyleBackColor = false;
+            this.buttonRose.Click += new System.EventHandler(this.buttonRose_Click);
+            // 
+            // buttonVert
+            // 
+            this.buttonVert.BackColor = System.Drawing.Color.YellowGreen;
+            this.buttonVert.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonVert.Location = new System.Drawing.Point(290, 149);
+            this.buttonVert.Name = "buttonVert";
+            this.buttonVert.Size = new System.Drawing.Size(199, 103);
+            this.buttonVert.TabIndex = 3;
+            this.buttonVert.Text = "Vert";
+            this.buttonVert.UseVisualStyleBackColor = false;
+            this.buttonVert.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(247, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Couleur";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 474);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(570, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(483, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "score";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label3.Location = new System.Drawing.Point(247, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // fontDialog2
+            // 
+            this.fontDialog2.Apply += new System.EventHandler(this.fontDialog2_Apply);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // tpsjeu
+            // 
+            this.tpsjeu.Enabled = true;
+            this.tpsjeu.Tick += new System.EventHandler(this.tpsjeu_Tick);
+            // 
+            // intervallecouleur
+            // 
+            this.intervallecouleur.Enabled = true;
+            this.intervallecouleur.Interval = 500;
+            this.intervallecouleur.Tick += new System.EventHandler(this.intervallecouleur_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 496);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Super Jeu concours";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBleu;
+        private System.Windows.Forms.Button buttonRouge;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonRose;
+        private System.Windows.Forms.Button buttonVert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button buttonrst;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.FontDialog fontDialog2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Timer tpsjeu;
+        private System.Windows.Forms.Timer intervallecouleur;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        //generated est un string qui désigne le nom de la couleur généré aléatoirement
+        //colorgen string globale, couleur généré aussi
+        //score le score du joueur
+
+
+        int score = 0;
+        string colorgen = "";
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        //bouton Bleu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ("Bleu" == colorgen)
+            {
+                score += 1;
+                label2.Text = "score : " + score.ToString();
+            }
+
+        }
+        //bouton Rouge
+        private void buttonRouge_Click(object sender, EventArgs e)
+        {
+           if ("Rouge" == colorgen)
+           {
+                score += 1;
+                label2.Text = "score : " + score.ToString();
+           }
+            
+        }
+        //bouton Rose
+        private void buttonRose_Click(object sender, EventArgs e)
+        {
+            if ("Rose" == colorgen)
+            {
+                score += 1;
+                label2.Text = "score : " + score.ToString();
+            }
+        }
+        //bouton Vert
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+            if ("Vert" == colorgen && j < 1)
+            {
+                score += 1;
+                label2.Text = "score : " + score.ToString();
+                
+            }
+        }
+        //bouton OK
+        private void buttonrst_Click(object sender, EventArgs e)
+        {
+            colorgen = fctgennum();
+            Score();
+        }
+
+        public int Score()
+        {
+
+            label2.Text = "score : " + score.ToString();
+            return score;
+        }
+
+        public string fctgennum()
+        {
+            string[] str = { "Rouge", "Bleu", "Rose", "Vert" };
+
+            Random rng = new Random();
+
+            int i = rng.Next(0, str.Length);
+            string generated = str[i];
+
+            //affiche le nom de la couleur
+            label1.Text = generated;
+
+            //retourne le nb généré
+            return generated;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int t = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        
+
+        private void fontDialog2_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        int time = 15;
+        private void tpsjeu_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time == 0)
+            {
+                DialogResult result = MessageBox.Show("TIME'S OUT!!" +
+                    " voulez-vous réessayer ??", "Un Titre", MessageBoxButtons.YesNo);
+                if (result == DialogResult.No)
+                {
+                    this.Close();
+                }
+                   
+            }
+        }
+
+        private void intervallecouleur_Tick(object sender, EventArgs e)
+        {
+            t++;
+            if (t % 2 == 1)
+            {
+
+                colorgen = fctgennum();
+            }
+        }
+    }
+}
